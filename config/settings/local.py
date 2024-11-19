75087ff558de0b9dd87f4e38b6a7903145defa82
Adesegun Koiki <kamar3deen@gmail.com>
2024-11-19 14:21:43 -0700

    feat: add logging to django for local dev

--- config/settings/local.py
+++ config/settings/local.py
@@ -17,8 +17,6 @@ SITE_NAME = getenv("SITE_NAME")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv("DJANGO_SECRET_KEY","n4KZEYCuq9PKSQrTFIjeZCpo9qScdeso7UEUiTXFjOtX-nFAzwc")
  
-
-
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 ADMIN_URL = getenv("DJANGO_ADMIN_URL")
@@ -27,3 +25,19 @@ EMAIL_HOST = getenv("EMAIL_HOST")
 EMAIL_PORT = getenv("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 DOMAIN = getenv("DOMAIN")
+
+LOGGING = {
+    "version":1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose":{
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers":{"console":{
+        "level": "DEBUG",
+        "class": "logging.StreamHandler",
+        "formatter": "verbose",
+    }},
+    "root": {"level": "INFO", "handlers":["console"]},
+}
